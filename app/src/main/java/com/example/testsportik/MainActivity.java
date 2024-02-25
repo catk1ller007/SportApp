82be35b2d1dffcbb375c7f06588a063a85c9d9c9
kylae <kylaev11@gmail.com>
2024-02-25 23:57:02 +0300

    first commit

--- app/src/main/java/com/example/testsportik/MainActivity.java
+++ app/src/main/java/com/example/testsportik/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Button button = findViewById(R.id.btnVoiti);
 
         button.setOnClickListener(new View.OnClickListener() {
